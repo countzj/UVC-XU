cafe699e0a59c6a10b60da1de1e9247efc942ff3
countzj <139815979@qq.com>
2017-08-03 17:29:16 +0800

    make文件小修改

--- Android.mk
+++ Android.mk
@@ -3,14 +3,14 @@ LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS  += -DLINUX
-
 LOCAL_MODULE:=cam_update
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/
 LOCAL_SRC_FILES:= update.cpp
 
-LOCAL_CFLAGS += -pie -fPIE
+LOCAL_CFLAGS += -pie -fPIE -std=c++11
 LOCAL_LDFLAGS += -pie -fPIE
 
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 include $(BUILD_EXECUTABLE)
 
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+
